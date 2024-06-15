9c87db8fe9025d6fb5c921c1bc5c2cb3df66097b
AbelOsaretin <contact.abel321@gmail.com>
2024-06-15 17:10:27 +0100

    appJs

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 import React, { useState } from 'react';
+import {Desk} from "./Components/Desk";
+import {BookingForm} from "./Components/BookingForm";
+
+
+
+const initialDesks = [
+  { id: 1, type: 'individual', booked: false }
+]
 
 function App() {
+  const [desks, setDesks] = useState(initialDesks);
+  const handleBooking = (deskId, hours, tier) => {
+    const updatedDesks = desks.map(desk =>
+      desk.id === deskId ? { ...desk, booked: true } : desk
+    );
+    setDesks(updatedDesks);
+
+
   return (
     // <div className="App">
     //   <header className="App-header">
